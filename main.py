cf1f4a81063474af3705710ca6ae879039c37c69
Aric <hernandoaric@gmail.com>
2020-01-17 21:26:25 +0700

    Final Version

--- main.py
+++ main.py
@@ -1,106 +1,142 @@
 import os
+import pygame
+import grid as g
+import time
+
 clear = lambda : os.system('cls')
 
-height : int = 3
-width  : int = 3
+# grids will contain grid objects
+grids = []
 
-grid = [["-" for x in range(width)] for y in range (width)]
+WHITE = (255,255,255)
+GREEN = (0,255,0)
+BLACK = (0,0,0)
+                
+# creates a grid object
+def initializeNewGrid():
+    gridDatas = {}
+    dataKeywords = ['gridHeight','gridWidth','cellSize']
 
-def displayGrid():
-    # Temporarily here
-    count = 0
-    space = "  "
-    print("+",end="\t")
-    for i in range(width):
-        if((i)>=10):
-            space=" "
-        print(i, end = space)
-
-    print()
-    for i in grid:
-        print(count,end="\t")
-        count+=1
-        for j in i:
-            print(j,end = "  ")
-        print()
-
-def setLive(x,y):
-    grid[x][y] = "X"
-
-
-# returns a list of neighbouring coordinates of the selected point
-def neighbourIterator(yCoord,xCoord) -> list:
-    neighbourCoordinates = []
-    yCoord -= 1
-    xCoord -= 1
-    for y in range(3):
-        for x in range(3):
-            if((yCoord+y<0 or yCoord+y>height-1) or (xCoord+x<0 or xCoord+x>width-1)):
+    # creates a dictionary, with the key originating from dataKeywords
+    # this dictionary will then be used as a keyword argument
+    for keyword in dataKeywords:
+        dataEntered = False
+        while not dataEntered:
+            clear()
+            print("Enter 0 for default value")
+            
+            # checks for invalid input 
+            try:
+                gridData = int(input("Enter " + keyword[:4] + " " + keyword[4:].lower() + " : "))
+                if(gridData != 0):
+                    gridDatas[keyword] = gridData
+                    dataEntered = True
+                else:
+                    dataEntered = True
+            except:
                 continue
-            neighbourCoordinates.append([yCoord+y,xCoord+x])
-
-    # deletes the source coordinate, since that isn't a part of the neighbouring cell
-    neighbourCoordinates.remove([yCoord+1,xCoord+1])
     
-    return neighbourCoordinates
+    # creates a grid object, with the information provided by the dictionary
+    grids.append(g.grid(**gridDatas))
 
-
-# returns an integer of surrounding live cell
-def neighbourCounter(yCoord,xCoord) -> int:
-    liveNeighbour = 0
-    for neighbour in neighbourIterator(yCoord,xCoord):
-        if(grid[neighbour[0]][neighbour[1]] == "X"):
-            liveNeighbour += 1
-    return liveNeighbour
-
-# runs the main loop to add cells that are alive    
+# runs the main loop
 while True:
     clear()
+    print("What would you like to do : ")
+    print("1. Add new grid")
+    print("2. View grids")
 
-    print("1. Input live cell")
-    print("2. Simulate next step")
-
-    option = int(input("Pick option no : "))
-    if(option == 1):
-        try:
-            a = int(input("Put in col no : "))
-            b = int(input("Put in row no : "))
-
-            # checks if the input is valid, border is subtracted by 1 to account for the array
-            if((a<0 or a>height-1) or (b<0 or b>width-1)):
-                print("Invalid Input")
-                input("Press Enter to continue")
-                clear()
-                continue
-
-            setLive(a,b)
-        except:
-            print("Invalid Input")
-            input("Press Enter to continue")
+    try:
+        option = int(input("Option no : "))
+        assert(option <= 2)
+        if(option == 1):
             clear()
+            initializeNewGrid()
+            grids[-1].setupGrid()    
+        elif(option == 2 and len(grids) != 0):
+            break
+        else:
+            print("Grid is empty")
+            input("Press Enter to continue")
             continue
+    except AssertionError:
+        print("Invalid input")
 
-    elif(option == 2):
-        nextGrid = []
-        print(id(nextGrid),id(grid))
-        for y in range(height):
-            nextGrid.append([])
-            for x in range(width):
-                liveNeighbour = neighbourCounter(y,x)
+# initializes pygame
+def pygameDisplay():
+    pygame.init()
+    done = False
+    tick = 0
+    clock = pygame.time.Clock()
+    
+    gridsIndex = 0
+    
+    # gets all of the data from the selected grid
+    gridArr = grids[gridsIndex].gridArr
+    gridWidth  = grids[gridsIndex].gridWidth
+    gridHeight = grids[gridsIndex].gridHeight
+    cellSize   = grids[gridsIndex].cellSize
+    cellMargin = grids[gridsIndex].cellMargin
+    cellWithMargin = cellSize + cellMargin
+    
+    while not done:
+        startTime = time.time()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+            # if space is pressed, change the grid
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                gridsIndex = gridsIndex + 1 if gridsIndex < len(grids)-1 else 0
                 
-                # checks if a live cell has either 2 or 3 live neighbours
-                if(grid[y][x] == "X" and liveNeighbour in range(2,4)):
-                    nextGrid[y].append("X")
-                    continue
-                elif(grid[y][x] == "-" and liveNeighbour == 3):
-                    nextGrid[y].append("X")
-                    continue
-                else:
-                    nextGrid[y].append("-")
-                    continue
-        grid = nextGrid[:]
-    displayGrid()
-    input("Press Enter to continue")
+                # gets all of the data from the selected grid
+                gridArr = grids[gridsIndex].gridArr
+                gridWidth  = grids[gridsIndex].gridWidth
+                gridHeight = grids[gridsIndex].gridHeight
+                cellSize   = grids[gridsIndex].cellSize
+                cellMargin = grids[gridsIndex].cellMargin
+                cellWithMargin = cellSize + cellMargin
 
-displayGrid()
+        # calculates the size of the screen thats going to be displayed
+        screenWidth = gridWidth * (cellWithMargin)
+        screenHeight = gridHeight * (cellWithMargin)
+        screen = pygame.display.set_mode((screenWidth,screenHeight))
+        
+        # fills the background
+        screen.fill(WHITE)
 
+        # added 1 so that the lines create a proper square.
+        # draws the line that separate the cells
+        verticalLinesDrawn = False
+        for y in range(gridHeight + 1):
+            # used so that the program doesn't keep creating vertical lines for every cell, only drawing it once
+            if(not verticalLinesDrawn):
+                for x in range(gridWidth + 1):
+                    pygame.draw.line(screen , BLACK , (x * cellWithMargin,0) , (x * cellWithMargin ,screenHeight))
+            verticalLinesDrawn = True
+            pygame.draw.line(screen , BLACK , (0,y * cellWithMargin) , (screenWidth,y * cellWithMargin))
+
+        # draws the cell into display
+        for y in range(gridHeight):
+            for x in range(gridWidth):
+                if(gridArr[y][x]):
+                    pygame.draw.rect(screen,GREEN,(cellWithMargin * x + cellMargin, cellWithMargin * y + cellMargin,cellSize,cellSize))
+                    continue
+        
+        # for every second, adds the number of generation, and gets the next generation of grid
+        tick += 1
+        simulationInterval = 60
+        if(tick % simulationInterval == 0):
+            tick = 0
+            # calculates the next generation of grid and replaces the old one
+            gridArr = grids[gridsIndex].nextGeneration()
+            grids[gridsIndex].gridArr = gridArr
+
+        clock.tick(60)
+
+        # displays the fps on the caption
+        fps = 1 / (time.time() - startTime)
+        pygame.display.set_caption("Conway's Game of Life. FPS: "+ (str(fps)))
+        pygame.display.flip()
+    pygame.quit()
+pygameDisplay()
